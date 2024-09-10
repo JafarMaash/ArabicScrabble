de30004b1b60aac976ab0a18ee89c81c35ef0135
JafarMaash <Jmaa831@aucklanduni.ac.nz>
2024-09-11 07:41:49 +1200

    Initial commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Board from './Components/Board/Board';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+
+
+          Welcome to Arabic scrabble
+        </p>
+       <Board />
+      </header>
+    </div>
+  );
+}
+
+export default App;
